ff9ae9e869ebc82c29469690713b996da9440cfd
Chelsjavis <chelsea.davis975@gmail.com>
2025-09-20 12:34:42 +1000

    Added footer, tidied up a few things

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">My weather Application</header>
+      <header className="App-header">
+        <h1>My weather Application</h1>
+      </header>
+      <footer>
+        This project was coded by Chelsea D, is{" "}
+        <a
+          href="https://github.com/Chelsjavis/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://github.com/Chelsjavis/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify.
+        </a>
+      </footer>
     </div>
   );
 }
